dae64dd2bc7ec93b7ad191ac41eb3aba0e091181
RomaLuch <luchinsky@list.ru>
2019-06-03 00:01:30 +0300

    chapter5: aop property watcher

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,18 @@
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import performers.Performer;
+import performers.MindReader;
+import performers.Thinker;
 
 public class Main {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-chapter5.xml");
-        Performer eddie = (Performer) context.getBean("eddie");
-        eddie.perform();
+        MindReader magician = (MindReader) context.getBean("magician");
+        Thinker volunteer = (Thinker) context.getBean("volunteer");
+
+        volunteer.thinkOfSomething("Queen of Hearts");
+
+        System.out.println("Queen of Hearts".equals(magician.getThoughts()));
     }
+
+
 }
--- src/main/java/performers/Magician.java
+++ src/main/java/performers/Magician.java
@@ -1,8 +1,20 @@
 package performers;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
 public class Magician implements MindReader {
+
+    @Pointcut("execution(* performers."
+            + "Thinker.thinkOfSomething(String)) && args(thoughts)")
+    public void thinking(String thoughts) {
+    }
+
     private String thoughts;
 
+    @Before("thinking(thoughts)")
     public void interceptThoughts(String thoughts) {
         System.out.println("Intercepting volunteer’s thoughts");
         this.thoughts = thoughts;
