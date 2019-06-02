a5dd6f75f529b515a5e28d67dff4213143898a57
RomaLuch <luchinsky@list.ru>
2019-06-02 22:05:07 +0300

    chapter5: aop anotations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,11 @@
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import performers.MindReader;
-import performers.Thinker;
+import performers.Performer;
 
 public class Main {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-chapter5.xml");
-        MindReader magician = (MindReader) context.getBean("magician");
-        Thinker volunteer = (Thinker) context.getBean("volunteer");
-
-        volunteer.thinkOfSomething("Queen of Hearts");
-
-        System.out.println("Queen of Hearts".equals(magician.getThoughts()));
+        Performer eddie = (Performer) context.getBean("eddie");
+        eddie.perform();
     }
-
-
 }
--- src/main/java/aop/Audience.java
+++ src/main/java/aop/Audience.java
@@ -1,23 +1,30 @@
 package aop;
 
-import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 
+@Aspect
 public class Audience {
 
-    public void watchPerformance(ProceedingJoinPoint joinpoint) {
-        try {
-            System.out.println("The audience is taking their seats.");
-            System.out.println("The audience is turning off their cellphones");
-            long start = System.currentTimeMillis(); // Перед выступлением
+    @Pointcut("execution(* performers.Performer.perform(..))")
+    public void performance() {
+    }
 
-            joinpoint.proceed(); // Вызов целевого метода
+    @Before("performance()")
+    public void takeSeats() { // Перед выступлением
+        System.out.println("The audience is taking their seats.");
+    }
 
-            long end = System.currentTimeMillis(); // После выступления
-            System.out.println("CLAP CLAP CLAP CLAP CLAP");
-            System.out.println("The performance took " + (end - start)
-                    + " milliseconds.");
-        } catch (Throwable t) {
-            System.out.println("Boo! We want our money back!");
-        }
+    @Before("performance()")
+    public void turnOffCellPhones() { // Перед выступлением
+        System.out.println("The audience is turning off their cellphones");
+    }
+
+    @AfterReturning("performance()")
+    public void applaud() { // После выступления
+        System.out.println("CLAP CLAP CLAP CLAP CLAP");
+    }
+    @AfterThrowing("performance()")
+    public void demandRefund() { // После неудачного выступления
+        System.out.println("Boo! We want our money back!");
     }
 }
